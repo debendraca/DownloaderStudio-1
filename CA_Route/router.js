c64675d44dec6003fc84ee7eebfbbdf8fd83d570
debendraca <debendraca@gmail.com>
2025-07-16 18:47:13 +0530

    Create router.js
    Added router.js file

---
+++ CA_Route/router.js
@@ -0,0 +1,7 @@
+/**
+ * ========================================
+ * File:        router.js
+ * Description: Defining All the Name of the router and adding event handler to it.
+ * Created:     2025-07-16
+ * ========================================
+ */
