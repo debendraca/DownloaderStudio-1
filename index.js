c6ae831cb677c268542cfb54dd23f9531714ca61
debendraca <debendraca@gmail.com>
2025-07-17 14:48:43 +0530

    Added home page and other packages

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const express=require('express');
 const bodyparser=require('body-parser');
 const caRouter=require('../CommunityApplication/ca_Router/router')
 require('dotenv').config();
+const path = require('path');
+const open=require('open');
+const hostname=require('os');
+
 
 
 const app=express();
 
 const PORT = process.env.PORT || 5000;
 
+//const filePath = path.join(__dirname, 'public', 'home.html');
 app.listen(PORT,()=>{
     console.log(`Server started at port ${PORT}` );
+    
+});
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Or send a specific HTML file manually
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 app.use(bodyparser.json());
 app.use('/api',caRouter);
